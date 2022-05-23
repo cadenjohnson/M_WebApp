4867d2b031518bd840009a99b45a8023d20afa82
cadenjohnson <caden82010@hotmail.com>
2022-05-22 22:47:46 -0400

    yeah buddy

---
+++ README.txt
@@ -0,0 +1,16 @@
+Start virtual environment via 
+.\M_env\Scripts\activate
+
+
+run app via
+python main.py
+
+
+update database via
+flask db migrate
+or
+flask db upgrade     ?
+
+
+
+Please update me thx
--- config.py
+++ config.py
@@ -6,5 +6,6 @@ BASE_DIR = os.path.dirname(os.path.abspath(__name__))
 
 class Config:
     DEBUG = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'database.db')
     SECRET_KEY = 'WUBULUBADUBDUB'
